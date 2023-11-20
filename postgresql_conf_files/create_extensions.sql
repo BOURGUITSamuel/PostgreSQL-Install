5934c737d901fb7fb24e8ad218c9249abe881291
Jean - Samuel BOURGUIT <65498199+BOURGUITSamuel@users.noreply.github.com>
2023-11-20 17:12:19 +0100

    Add files via upload

---
+++ postgresql_conf_files/create_extensions.sql
@@ -0,0 +1,6 @@
+CREATE EXTENSION IF NOT exists "uuid-ossp";
+CREATE EXTENSION IF NOT exists "orafce";
+CREATE EXTENSION IF NOT exists "pgcrypto";
+CREATE EXTENSION IF NOT exists "uuid-ossp";
+CREATE EXTENSION IF NOT exists "unaccent";
+\q
